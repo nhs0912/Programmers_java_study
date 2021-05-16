29976b66d656f029aab35e0b9916931d8214e2ad
HeeSeok Noh <nhs0912@nate.com>
2021-05-16 23:55:37 +0900

    feat: 모의고사 문제 완성

--- src/main/java/step1/solution3/Solution1_3.java
+++ src/main/java/step1/solution3/Solution1_3.java
@@ -1,6 +1,7 @@
 package step1.solution3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution1_3 {
@@ -8,12 +9,11 @@ public class Solution1_3 {
     private static final int[] firstHumanAnswer = new int[]{1, 2, 3, 4, 5};
     private static final int[] secondHumanAnswer = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
     private static final int[] thirdHumanAnswer = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    private static final int[] memberAnswerCountArray = new int[3];
 
     public int[] solution(int[] answers) {
-        int[] answer = {};
-
-
-        return answer;
+        saveCorrectAnswerCount(answers);
+        return highestScoreMember().stream().mapToInt(Integer::intValue).toArray();
     }
 
     public List<Integer> loopAnswer(int problemCount, int[] answers) {
@@ -26,6 +26,54 @@ public class Solution1_3 {
             }
         }
         return answerList;
+    }
+
+    private void saveCorrectAnswerCount(int[] answers) {
+        int answersSize = answers.length;
+
+        List<Integer> firstHumanAnswerList = loopAnswer(answersSize, firstHumanAnswer);
+        List<Integer> secondHumanAnswerList = loopAnswer(answersSize, secondHumanAnswer);
+        List<Integer> thirdHumanAnswerList = loopAnswer(answersSize, thirdHumanAnswer);
+
+        memberAnswerCountArray[0] = correctedAnswerCount(answers, firstHumanAnswerList);
+        memberAnswerCountArray[1] = correctedAnswerCount(answers, secondHumanAnswerList);
+        memberAnswerCountArray[2] = correctedAnswerCount(answers, thirdHumanAnswerList);
+    }
+
+    private List<Integer> highestScoreMember() {
+        int bigNumber = bigCount();
+        List<Integer> highestMemberList = new ArrayList<>();
+        for (int i = 0; i < memberAnswerCountArray.length; i++) {
+            int correctedAnswerCount = memberAnswerCountArray[i];
+            if (correctedAnswerCount == bigNumber) {
+                highestMemberList.add(i + 1);
+            }
+        }
+        return highestMemberList;
+    }
+
+
+    private int bigCount() {
+        int bigNumber = -1;
+        for (int i = 0; i < memberAnswerCountArray.length; i++) {
+            if (bigNumber < memberAnswerCountArray[i]) {
+                bigNumber = memberAnswerCountArray[i];
+            }
+        }
+        return bigNumber;
+    }
+
+
+    public int correctedAnswerCount(int[] answers, List<Integer> humanAnswers) {
+        int answerCount = 0;
+        for (int i = 0; i < answers.length; i++) {
+            answerCount = oneAddedCount(humanAnswers.get(i), answers[i], answerCount);
+        }
+        return answerCount;
+    }
+
+    private int oneAddedCount(int humanAnswer, int answer, int answerCount) {
+        return humanAnswer == answer ? answerCount + 1 : answerCount;
     }
 
 }
--- src/test/java/step1/solution3/Solution1_3Test.java
+++ src/test/java/step1/solution3/Solution1_3Test.java
@@ -5,6 +5,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,18 +14,45 @@ class Solution1_3Test {
     private static final int[] firstHumanAnswer = new int[]{1, 2, 3, 4, 5};
     private static final int[] secondHumanAnswer = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
     private static final int[] thirdHumanAnswer = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    private Solution1_3 s = new Solution1_3();
+
+    @Test
+    @DisplayName("mainTest")
+    void mainTest(){
+        int[] answers = new int[]{1, 2, 3, 4, 5};
+        assertThat(s.solution(answers)).containsExactly(1);
+
+        int[] answers2 = new int[]{1, 3, 2, 4, 2};
+        assertThat(s.solution(answers2)).containsExactly(1,2,3);
+    }
 
     @Test
     @DisplayName("숫자만큼 정답을 loop도는지 확인")
     void loopTest() {
-        Solution1_3 s = new Solution1_3();
+
         assertAll(
                 () -> assertThat(s.loopAnswer(3, firstHumanAnswer)).containsExactly(1, 2, 3),
                 () -> assertThat(s.loopAnswer(3, secondHumanAnswer)).containsExactly(2, 1, 2),
                 () -> assertThat(s.loopAnswer(3, thirdHumanAnswer)).containsExactly(3, 3, 1)
         );
-
-
     }
+
+    @Test
+    @DisplayName("정답과 비교하는 test")
+    void compareAnswerTest() {
+
+        int[] answers = new int[]{1, 2, 3, 4, 5};
+        int answersSize = answers.length;
+        List<Integer> firstHumanAnswerList = s.loopAnswer(answersSize, firstHumanAnswer);
+        List<Integer> secondHumanAnswerList = s.loopAnswer(answersSize, secondHumanAnswer);
+        List<Integer> thirdHumanAnswerList = s.loopAnswer(answersSize, thirdHumanAnswer);
+
+        assertAll(
+                () -> assertThat(s.correctedAnswerCount(answers, firstHumanAnswerList)).isEqualTo(5),
+                () -> assertThat(s.correctedAnswerCount(answers, secondHumanAnswerList)).isEqualTo(0),
+                () -> assertThat(s.correctedAnswerCount(answers, thirdHumanAnswerList)).isEqualTo(0)
+        );
+    }
+
 
 }
